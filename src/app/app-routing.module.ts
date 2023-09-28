5a8611b594ea16d60de864f38667adcdeb409816
Hasan Mesbaul Ali Taher <hasanmesbaul420@gmail.com>
2023-09-28 10:06:24 +0600

    Done part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Level1Component } from './level1/level1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"level1", component: Level1Component},
+{path:'', pathMatch:'full', redirectTo:'level1'}  
+//if no path, then go to the path of 'level1'
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
