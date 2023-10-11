7a95fb4a4e6f72e71bc2cc8700c6c0b6befc9cb4
Hasan Mesbaul Ali Taher <hasanmesbaul420@gmail.com>
2023-10-11 15:41:06 +0600

    Level 4 completed

--- src/app/level4/board.service.ts
+++ src/app/level4/board.service.ts
@@ -4,13 +4,92 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BoardService {
+  // // Demo
+  // x:number = 1;
+  // y:number = 1;
+  // r:number = 200;
 
-  x:number = 1;
-  y:number = 1;
-  r:number = 200;
+  // add(){
+  //   this.r = this.x + this.y;
+  // }
+  private currentPlayerIx!: number;
+  private currentWinnerIx!: number;
+  public boardContent!: number[][];
 
-  add(){
-    this.r = this.x + this.y;
+  constructor(){
+    this.restart();
   }
-  constructor() { }
+  
+  public restart(){
+    this.boardContent = [
+      [0 ,0 ,0 ],
+      [0 ,0 ,0 ],
+      [0 ,0 ,0 ],
+    ];
+    this.currentPlayerIx = 1;
+    this.currentWinnerIx = 0;
+
+  }
+  
+  /**
+   * Gets the player (1 or 2) who has won the game
+   * @returns player (1 or 2) who has won, or 0 if there is no winner
+   */
+  private getWinnerIndex():number{
+    // Check rows
+    for (let row = 0; row < 3; row++) {
+      const first = this.boardContent[row][0];
+      if (
+        first !== 0 &&
+        this.boardContent[row][1] === first &&
+        this.boardContent[row][2] === first
+      ) {
+        return first;
+      }
+    }
+
+    // Check columns
+    for (let col = 0; col < 3; col++) {
+      const first = this.boardContent[0][col];
+      if (
+        first !== 0 &&
+        this.boardContent[1][col] === first &&
+        this.boardContent[2][col] === first
+      ) {
+        return first;
+      }
+    }
+
+    // Check diagonals
+    const first = this.boardContent[1][1];
+    if (first !== 0) {
+      if (
+        this.boardContent[0][0] === first &&
+        this.boardContent[2][2] === first
+      ) {
+        return first;
+      }
+      if (
+        this.boardContent[2][0] === first &&
+        this.boardContent[0][2] === first
+      ) {
+        return first;
+      }
+    }
+    return 0;  
+  }
+
+  public get winnerIndex():number{
+    return this.currentWinnerIx;
+  }
+
+  public set(col:number, row:number): void{
+    if(this.currentWinnerIx === 0 && this.boardContent[row][col] === 0){
+      this.boardContent[row][col] = this.currentPlayerIx;
+      this.currentPlayerIx = this.currentPlayerIx === 1 ? 2 : 1;
+    }
+    this.currentWinnerIx = this.getWinnerIndex();
+  }
+
+
 }
--- src/app/level4/level4.component.ts
+++ src/app/level4/level4.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BoardService } from './board.service';
+import { BoardCell } from '../level3/level3.component';
 
 @Component({
   selector: 'app-level4',
@@ -7,12 +8,43 @@ import { BoardService } from './board.service';
   styleUrls: ['./level4.component.scss']
 })
 export class Level4Component {
-    constructor(public board:BoardService){
+  // // Demo
+  // constructor(public board:BoardService){}
+  // getColor(){
+  //   return this.board.r > 100 ? 'red' : 'black';
+  // }
 
+  private playerNames: string[];
+
+  constructor(public board:BoardService){
+    this.playerNames = ['', 'X','O'];
+  }
+  
+  public getPlayerName(col:number, row:number): string{
+    return this.playerNames[this.board.boardContent[row][col]];
+  }
+
+  public getStyle(col:number, row:number): string{
+    return `occupied-${this.getPlayerName(col,row)}`
+  }
+
+  public getWinnerName(): string{
+    return this.playerNames[this.board.winnerIndex]
+  }
+
+  public getCells(): BoardCell[][] {
+    const result: BoardCell[][] = [];
+    for (let row = 0; row < 3; row++) {
+      result.push([]);
+      for (let col = 0; col < 3; col++) {
+        result[row][col] = {
+          playerName: this.getPlayerName(col, row),
+          class: this.getStyle(col, row),
+        };
+      }
     }
 
-    getColor(){
-      return this.board.r > 100 ? 'red' : 'black';
-    }
-    
+    return result;
+  }
+
 }
